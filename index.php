56392f7a1aa43218e3ad0a3809332bb0696ea7e8
olivierpetitportable <o.petit5@laposte.net>
2025-03-17 13:27:48 +0100

    responsive1703

--- index.php
+++ index.php
@@ -10,62 +10,73 @@
 <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
 </head>
 
+<body>
+  <header>
+    <div class="logo">
+      <img src="img/logo_200px.png" alt="logo ecoride">
+    </div>
+    <nav class="navbar">
+        <ul>
+          <li><a class="active" href="#">Page d'accueil</a></li>
+          <li><a href="#">Accès au covoiturages</a></li>
+          <li><a href="#">Connexion</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+   </nav>
+   <div class="menu_hamburger"><img src="img/bouton_burger.png" alt="menu hamburger"></div>
+  </header>
 
-<header>
-  <div class="logo">
-    <img src="img/logo_120.gif" alt="ecoride">
-  </div>
-  <nav>
-    <ul>
-      <li><a class="click" href="#">Page d'accueil</a></li>
-      <li><a href="#">Accès au covoiturages</a></li>
-      <li><a href="#">Connexion</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>
-  </nav>
-</header>
-
-<main>
+  <main>
   
-<section class="container">
-  <div class="texte">
-    <p>
-      <b>EcoRide</b> est une <b>startup française</b> récemment créée avec une mission claire 
-     <b>réduire l'impact environnemental des déplacements en favorisant le covoiturage</b>. 
-     En adoptant une approche écologique, <b>EcoRide propose une solution innovante</b>. 
-    </p>
-    <p>
-      <b>L’ambition d’EcoRide est de devenir la référence en matière de covoiturage pour les voyageurs 
-      soucieux de l’environnement</b> ainsi que ceux recherchant une alternative économique. 
-      La plateforme se concentre exclusivement sur les trajets en voiture, 
-      garantissant ainsi une expérience optimisée pour ses utilisateurs.
-    </p>
-    <p>
-      <b>Avec EcoRide, chaque trajet devient un pas de plus vers une mobilité plus responsable et durable.</b>
-    </p>
-  </div>
-  <div class="columnright">
-    <div class="illustration">
-      <img src="img/illust_463.jpg" alt="illustration">
+  <section class="container">
+    <div class="texte">
+      <p class="para">
+        <b>EcoRide</b> est une <b>startup française</b> récemment créée avec une mission claire 
+        <b>réduire l'impact environnemental des déplacements en favorisant le covoiturage</b>. 
+        En adoptant une approche écologique, <b>EcoRide propose une solution innovante</b>. 
+      </p>
+      <p class="para">
+       <b>L’ambition d’EcoRide est de devenir la référence en matière de covoiturage pour les voyageurs 
+        soucieux de l’environnement</b> ainsi que ceux recherchant une alternative économique. 
+        La plateforme se concentre exclusivement sur les trajets en voiture, 
+        garantissant ainsi une expérience optimisée pour ses utilisateurs.
+     </p>
+     <p class="para">
+       <b>Avec EcoRide, chaque trajet devient un pas de plus vers une mobilité plus responsable et durable.</b>
+     </p>
     </div>
-    <div class="recherche">
-    <p><b>Votre prochaine destination</b></p>
-    <form action="#">
-      <input class="accueil" type="text" placeholder="Votre destination" name="search">
-      <button class="accueil" type="button">Rechercher</button></li>
-    </form>
-  </div>
-</section>
+    <div class="columnright">
+      <div class="illustration_accueil">
+        <img src="img/illust_463.jpg" alt="illustration">
+      </div>
+     <div class="recherche">
+      <p><b>Votre prochaine destination</b></p>
+     <form action="#">
+       <input class="accueil" type="text" placeholder="Votre destination" name="search">
+       <button class="accueil" type="button">Rechercher</button></li>
+     </form>
+   </div>
+  </section>
 
-</main>
+  </main>
 
-<footer>
-    <div class="position_email">
-      <a class="email" href="#">ecoride@gmail.com</a>
-    </div>
-    <div class="position_mention">
-      <a class="mention" href="mention_legal.html" target="_blank">mention légal</a>
-    </div>
-</footer>
+  <footer>
+      <div class="position_email">
+       <a class="email" href="#">ecoride@gmail.com</a>
+      </div>
+      <div class="position_mention">
+        <a class="mention" href="mention_legal.html" target="_blank">mention légal</a>
+      </div>
+  </footer>
+</body>
+  <script>
+      const menuHamburger = document.querySelector(".menu_hamburger")
+      const navLinks = document.querySelector(".navbar")
+
+      menuHamburger.addEventListener('click',()=>{
+        navLinks.classList.toggle('mobile_menu')
+      })
+  </script>
+   
 
 </html>
