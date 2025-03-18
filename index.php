d8f22d29908765ced9bdaf379967a6ce6009114f
olivierpetitportable <o.petit5@laposte.net>
2025-03-18 15:20:36 +0100

    responsive 1803 portable

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/style_responsive.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
@@ -28,20 +28,20 @@
             <div class="menu_hamburger"><img src="img/bouton_burger.png" alt="menu hamburger"></div>
     </header>
 
-    <section id="presentation">
+    <section class="presentation">
         <article>
-            <p class="para">
+            <p class="paragraphe">
                 <b>EcoRide</b> est une <b>startup française</b> récemment créée avec une mission claire 
                 <b>réduire l'impact environnemental des déplacements en favorisant le covoiturage</b>. 
                 En adoptant une approche écologique, <b>EcoRide propose une solution innovante</b>. 
             </p>
-            <p class="para">
+            <p class="paragraphe">
                <b>L’ambition d’EcoRide est de devenir la référence en matière de covoiturage pour les voyageurs 
                 soucieux de l’environnement</b> ainsi que ceux recherchant une alternative économique. 
                 La plateforme se concentre exclusivement sur les trajets en voiture, 
                 garantissant ainsi une expérience optimisée pour ses utilisateurs.
             </p>
-            <p class="para">
+            <p class="paragraphe">
                <b>Avec EcoRide, chaque trajet devient un pas de plus vers une mobilité plus responsable et durable.</b>
             </p>
         </article>
