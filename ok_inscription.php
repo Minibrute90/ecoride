eebcef55f3665e112ed4cc0fd78e7d030e953030
olivierpetitportable <o.petit5@laposte.net>
2025-03-24 16:55:30 +0100

    maj bibliU 24/03

---
+++ ok_inscription.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <title>EcoRide</title>
+</head>
+
+<body class="site">
+    <header>
+    <div class="first_rows">
+        <div class="logo">
+            <img src="img/logo_200px.png" alt="logo ecoride">
+        </div>
+        <nav class="navbar">
+            <ul>
+                <li><a class="active" href="index.php">Page d'accueil</a></li>
+                <li><a href="accesaucovoiturages.php">Accès au covoiturages</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="second_rows">
+            <nav class ="user">
+                <ul>
+                    <li><a href="inscription.php">Inscrivez-vous</Inscrivez-vous></a></li>
+                    <li><a href="connection.php">Connectez-vous</a></li>
+                <ul>
+            </nav>
+            <div class="menu_hamburger"><img src="img/bouton_burger.png" alt="menu hamburger"></div>  
+    </div>
+    </header>
+
+    <section class="zone_user">
+
+        <article class="zone_user">
+
+                <h1 class="user_info">. Inscription reussie </h1>
+
+                    <div class="anim_pouce">
+                        <img src="img/pouce_400px.png" alt="inamation du pouve ok incription reussite" width="150px">
+                    </div>
+
+                <div class="bouton_user">
+                        <button type="button">Mon espace</button>
+                </div>
+    
+    </section>
+
+    <footer>
+        <div class="bt_footer">
+            <div class="position_email">
+                <a class="email" href="#">ecoride@gmail.com</a>
+            </div>
+            <div class="position_mention">
+                <a class="mention" href="mention_legal.html" target="_blank">mention légal</a>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+<script>
+    const menuHamburger = document.querySelector(".menu_hamburger")
+    const navLinks = document.querySelector(".navbar")
+
+    menuHamburger.addEventListener('click',()=>{
+      navLinks.classList.toggle('mobile_menu')
+    })
+</script>
+
+</html>
